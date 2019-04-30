1003a453878eb83a430971609d94eb31e313593b
napster <napster@localhost.localdomain>
2019-04-29 23:54:25 -0300

    terminado el punto 1

--- tierraDeBarbaros.hs
+++ tierraDeBarbaros.hs
@@ -4,18 +4,24 @@ import Data.Char
 type Objeto = Barbaro -> Barbaro
 data Barbaro = Barbaro { nombre :: String,
                          fuerza :: Int,
-                         habilidades :: [String]
---                         objetos :: [Objeto]
+                         habilidades :: [String],
+                         objetos :: [Objeto]
                         }deriving Show
 dave = Barbaro { nombre = "dave",
                  fuerza = 100,
-                 habilidades = ["tejer","escribirPoesia"]
---                 objetos = [ardilla, libroPedking]
+                 habilidades = ["tejer","escribirPoesia"],
+                 objetos = [ardilla]
                 }
 
 {-consulta, esta parte del codigo tiene 3 veces logica repetida... como se elimina?-}
 modificarFuerza funcion barbaro = barbaro {fuerza = (funcion.fuerza)barbaro}
-modificarHabilidades funcion barbaro = barbaro {fuerza = (funcion.fuerza)barbaro}
-modificarObjetos funcion barbaro = barbaro {fuerza = (funcion.fuerza)barbaro}
+modificarHabilidades funcion barbaro = barbaro {habilidades = (funcion.habilidades)barbaro}
+--modificarObjetos funcion barbaro = barbaro {objetos = (funcion.objetos)barbaro}
 
 espada peso = modificarFuerza (+(2*peso))
+amuletoMistico habilidad = modificarHabilidades (++[habilidad])
+agregarHacerMagia = amuletoMistico "hacerMagia"
+vaciarObjetos barbaro = barbaro{objetos = []}
+varitasDefectuosas = agregarHacerMagia.vaciarObjetos
+ardilla = id
+cuerda primerObjeto segundoObjeto = primerObjeto.segundoObjeto
